85abcd968be317c81d8ee7ec49cf31b402947b60
Grant Schowalter <grantschowalter@gmail.com>
2021-04-01 13:07:08 -0400

    Changed system interface to use ros

--- system_interface.py
+++ system_interface.py
@@ -1,6 +1,9 @@
 # This module is written to be run using the python2.7 interpreter to 
 # connect with ros Melodic
 import socket
+import rospy
+from std_msgs.msg import Int8, String, Bool
+from navigation_msgs import VehicleState, Stop
 
 host = ''
 port = 2500
@@ -10,11 +13,25 @@ sock.listen(1)
 c, addr = sock.accept()     # Establish connection with client.
 print('Got connection from: {}', addr)
 
+class sys_interface(object):
+   def __init__(self):
+        rospy.init_node('speech_detector')
+        self.speech_text = rospy.Publisher('/speech_text', String, queue_size=10)
+        self.stop_cart = rospy.Publisher('/stop', Stop, queue_size=10)
+        cart_stopped = False
+   
+   def anounce(self, text):
+      print("Publishing")
+      self.speech_text.publish(text)
+
+anouncer = sys_interface()
+
 while True:
    text = c.recv(2048)
    fo = open("Log.txt", "w")
    fo.write(text.decode())
    print("Data: %s" %text.decode())
-   if("stop" in text.decode()):
+   anouncer.anounce(text.decode())
+   if("halt" in text.decode()):
       c.close()                # Close the connection
       break
